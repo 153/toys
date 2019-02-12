58ccf4944aac7919d8ff70f135831c5471ab4d79
153 <x153@live.com>
2019-02-11 21:34:32 -0800

    Update cipher.py
    format enciphered text into 5 letter blocks of characters //  more idiomatic control flow

--- cipher.py
+++ cipher.py
@@ -1,7 +1,15 @@
 # NSA "Diana" cipher
 # using "autokey" algorithm
+import random
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+def main():    
+    dec, plain, keyword = get_terms()
+    cipher = handle_terms(dec, plain, keyword)
+    cipher = wordblocks(cipher) if not dec else cipher
+    print("-"*40)
+    print("input:", plain, "\nkey  :", keyword)
+    print("----->", cipher)
 
 def formatstr(text):
     """Extract all letters from a string and make them uppercase"""
@@ -13,41 +21,45 @@ def getpos(inp):
 
 def encode(pt='', ct=''):
     """Vastly simplified DIANA algorithm"""
-    if (25 - getpos(pt) - getpos(ct)) < 0:
-        return alphabet[51 - getpos(pt) - getpos(ct)]
-    else:
-        return alphabet[25 - getpos(pt) - getpos(ct)]
+    return alphabet[(25 - getpos(pt) - getpos(ct)) % 26]
 
 def get_terms():
     """Users input a message and key, then encrypt/decrypt."""
-    dec = input("Decode?\n[0/1] ")
+    dec = 0 if input("Decode?\n[0/1] ") is "0" else 1
     plain = input("Please enter your message...\n> ")
     keyword = input("Please enter your key...\n> ")
-    if dec is "0":
-        dec = 0
     return dec, formatstr(plain), formatstr(keyword)
 
 def handle_terms(dec, plain, keyword):
     """Encrypt or decrypt a message using autokey style"""
     cipher = []
-    if not dec:
-        keyword = keyword + plain
+    if not dec: # aka Enciphering
+        keyword += plain
         cipher = [encode(letter, keyword[n]) \
                   for n, letter in enumerate(plain)]
-    else:
+    else: # Deciphering -> appending cleartext to key as it is deciphered
         for n, letter in enumerate(plain):
             letter = encode(letter, keyword[n])
             cipher.append(letter)
-            keyword += letter            
-    return "".join(cipher)    
+            keyword += letter
+    return "".join(cipher)
 
-def main():    
-    dec, plain, keyword = get_terms()
-    cipher = handle_terms(dec, plain, keyword)
-    
-    print("-"*40)
-    print("input:", plain, "\nkey  :", keyword)
-    print("----->", cipher)
+def pad(num):
+    """pad a message with num random characters""" 
+    output = ""
+    while len(output) < (5-num):
+        output += random.choice(alphabet)
+    return output
+
+def wordblocks(text):
+    """Format encoded output as blocks of 5 characters"""
+    i, text2 = 0, ""
+    if len(text) % 5:
+        text += pad(len(text) % 5)
+    while i < len(text):
+        i += 5
+        text2 += text[i-5:i] + " "
+    return text2
 
 if __name__ == "__main__":
     while True:
