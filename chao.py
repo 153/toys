67ee14bae10fab97971b7ce36d4519007f5447e3
153 <x153@live.com>
2019-02-25 03:14:28 -0800

    Update chao.py

--- chao.py
+++ chao.py
@@ -1,32 +1,29 @@
-# Python3 implementation of Chaocipher
-# L: PYTHONFLAVREDZXWUSQMKJIGCB (cipherwheel)
-# R: CHAOIPERZYXWVUTSQNMLKJGFDB (plain wheel)
-# 
-# -> PYXMHGALDVNVHSYBNJETMXDZPFL
-# <- CONGRATULATIONSONCRACKINGME
+# Python3 implementation of the Chaocipher
+# Left wheel  = ciphertext wheel
+# Right wheel = plaintext  wheel
 
 def main():
-    # letters only! 
-    lalpha = makealpha("pythonflavored")
-    ralpha = makealpha("chaocipher") 
-    
-    mymsg  = "congratulations on cracking me"
-    cipher = "PYXMHGALDVNVHSYBNJETMXDZPFL"
-    
+    # letters only! makealpha(key) helps generate lalpha/ralpha. 
+    lalpha = "HXUCZVAMDSLKPEFJRIGTWOBNYQ"
+    ralpha = "PTLNBQDEOYSFAVZKGJRIHWXUMC"
+    msg = "WELLDONEISBETTERTHANWELLSAID"
+
     print("L:", lalpha)
     print("R:", ralpha)
-    print()
-    print("->", do_chao(mymsg, lalpha, ralpha))
-    print("<-", do_chao(cipher, lalpha, ralpha, 0))
+    print("I:", msg)
+    print("O:", do_chao(msg, lalpha, ralpha, 1, 0), "\n")
+    
+    do_chao(msg, lalpha, ralpha, 1, 1)
 
-def correct_case(string):
-    return "".join([s.upper() for s in string if s.isalpha()])
-
-def do_chao(msg, lalpha, ralpha, en=1):
+def do_chao(msg, lalpha, ralpha, en=1, show=0):
     msg = correct_case(msg)
-    out = ""
+    out = ""    
+    if show:
+        print("="*54)        
+        print(10*" " + "left:" + 21*" " + "right: ")
+        print("="*54)        
+        print(lalpha, ralpha, "\n")
     for L in msg:
-        pos = ralpha.index(L)
         if en:
             lalpha, ralpha = rotate_wheels(lalpha, ralpha, L)
             out += lalpha[0]
@@ -34,6 +31,8 @@ def do_chao(msg, lalpha, ralpha, en=1):
             ralpha, lalpha = rotate_wheels(ralpha, lalpha, L)
             out += ralpha[0]
         lalpha, ralpha = scramble_wheels(lalpha, ralpha)
+        if show:
+            print(lalpha, ralpha)            
     return out
     
 def makealpha(key=""):
@@ -42,6 +41,9 @@ def makealpha(key=""):
     key = [x.upper() for x in (key + alpha[::-1])
            if not (x.upper() in z or z.add(x.upper()))]
     return "".join(key)
+
+def correct_case(string):
+    return "".join([s.upper() for s in string if s.isalpha()])
 
 def permu(alp, num):
     alp = alp[:num], alp[num:]
